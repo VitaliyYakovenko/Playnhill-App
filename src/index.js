ee4c4e0044aa5bd221a06b4b786d9452a5099579
VitaliyYakovenko <yakovenkovitaly1@gmail.com>
2023-04-23 17:54:52 +0300

    add commit - 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <BrowserRouter basename='/Adital-App'>
+    <BrowserRouter basename='/Playnhill-App'>
       <App />
     </BrowserRouter>
   // </React.StrictMode>
