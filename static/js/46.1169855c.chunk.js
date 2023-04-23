8f83bbc4df8f5d188743dacb24bbb1a87a56f9d0
VitaliyYakovenko <111122626+VitaliyYakovenko@users.noreply.github.com>
2023-04-23 14:53:50 +0000

    Deploying to gh-pages from @ VitaliyYakovenko/Adital-App@9c90eddd94c080da256ee5b5d9dbfcbb2c7ac5e3 🚀

---
+++ static/js/46.1169855c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[46],{46:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("div",{children:"Technologies"})}}}]);
+//# sourceMappingURL=46.1169855c.chunk.js.map
---
+++ static/js/613.263f0117.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[613],{613:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(184);function a(){return(0,r.jsx)("div",{children:"Detailed information"})}}}]);
+//# sourceMappingURL=613.263f0117.chunk.js.map
