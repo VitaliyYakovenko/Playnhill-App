efbdc4b90bf4d8e15379db355e9598a13ea14ef9
VitaliyYakovenko <111122626+VitaliyYakovenko@users.noreply.github.com>
2023-09-17 12:08:12 +0000

    Deploying to gh-pages from @ VitaliyYakovenko/Playnhill-App@9fad5d6fbbb0304b582f05d845995e20fee1d067 🚀

--- static/js/373.21c48eb5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[373],{9373:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("div",{children:"Ganres"})}}}]);
-//# sourceMappingURL=373.21c48eb5.chunk.js.map
